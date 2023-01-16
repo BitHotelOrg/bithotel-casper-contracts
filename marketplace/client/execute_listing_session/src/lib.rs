119849a203dfe7fbf31c3ae803bb9c864dddca70
Bufo <bufo24@users.noreply.github.com>
2023-01-16 16:28:32 +0100

    fix linter

--- marketplace/client/execute_listing_session/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![no_std]
-#![no_main]
-extern crate alloc;
-
-mod call;
-mod structs;
-
-pub type Time = u64;
-pub type TokenId = u64;
--- marketplace/client/execute_listing_session/src/main.rs
+++ marketplace/client/execute_listing_session/src/main.rs
@@ -5,50 +5,33 @@
 compile_error!("target arch should be wasm32: compile with '--target wasm32-unknown-unknown'");
 
 extern crate alloc;
-use core::ptr::null;
-
-use alloc::string::{String, ToString};
 
 use casper_contract::{
-    contract_api::{
-        account, runtime, storage,
-        system::{self, create_purse, transfer_from_purse_to_account},
-    },
+    contract_api::{account, runtime, system},
     unwrap_or_revert::UnwrapOrRevert,
 };
-use casper_types::{
-    account::AccountHash,
-    api_error::ApiError,
-    bytesrepr::{FromBytes, ToBytes},
-    runtime_args,
-    system::mint::Error,
-    CLTyped, ContractHash, Key, RuntimeArgs, URef, U512,
-};
-
-use crate::structs::{dict::Dict, order::Listing};
+use casper_types::{runtime_args, ContractHash, Key, RuntimeArgs, U512};
 
 const ARG_MARKETPLACE_CONTRACT_HASH: &str = "marketplace_contract_hash";
 const ARG_LISTING_ID: &str = "listing_id";
 const ARG_AMOUNT: &str = "amount";
 const ARG_PURSE: &str = "purse";
 const ENTRY_POINT_EXECUTE_LISTING: &str = "execute_listing";
-const ENTRY_POINT_GET_LISTING: &str = "get_listing";
 /**
  * TODO: call execute listing from marketplace contract
  */
 
 #[no_mangle]
 pub extern "C" fn call() {
-    let amount: U512 = runtime::get_named_arg("amount");
+    let amount: U512 = runtime::get_named_arg(ARG_AMOUNT);
 
     let marketplace_contract_hash: ContractHash =
         runtime::get_named_arg::<Key>(ARG_MARKETPLACE_CONTRACT_HASH)
             .into_hash()
-            .map(|hash| ContractHash::new(hash))
+            .map(ContractHash::new)
             .unwrap();
     let listing_id: u64 = runtime::get_named_arg(ARG_LISTING_ID);
 
-    let caller_purse = account::get_main_purse();
     let new_purse = system::create_purse();
 
     // Transfer from the caller's main purse to the new purse that was just created.
