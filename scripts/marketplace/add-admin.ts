1bc5e59c705440c3a24c30e4082adbb5ad748c42
Bufo <bufo24@users.noreply.github.com>
2023-08-28 12:31:03 +0200

    feat: add add-admin to new env structure

--- scripts/marketplace/add-admin.ts
+++ scripts/marketplace/add-admin.ts
@@ -6,21 +6,19 @@ import {
   CLKey,
   Keys,
   CLValueBuilder,
+  CLKeyBytesParser,
+  CLPublicKey,
+  CLValue,
+  CLType,
+  CLKeyType,
 } from "casper-js-sdk";
 import { CasperHelpers } from "./helpers";
+import { getEnvironmentVars } from "../env";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-let rpcUri: string;
-let privateKey: string;
-
-if (process.env.RPC_URI && process.env.PRIVATE_KEY) {
-  rpcUri = process.env.RPC_URI;
-  privateKey = process.env.PRIVATE_KEY;
-} else {
-  throw new Error(`No rpcUri or privateKey found`);
-}
+const { rpcUri, marketplace, chainName } = getEnvironmentVars();
 
 const casperClient = new CasperClient(rpcUri);
 
@@ -29,13 +27,11 @@ const privateKeyPath = "/Users/bufo/Downloads/BitHotel_secret_key.pem";
 const key = Keys.Ed25519.loadKeyPairFromPrivateFile(privateKeyPath);
 
 const contractClient = new Contracts.Contract();
-contractClient.setContractHash(
-  "hash-411f0c5ae537b18beabfe43601d3e2d238bd7b89e1566ff78fff66a07826a0db"
-);
+contractClient.setContractHash(marketplace);
 
 const runtimeArgs = RuntimeArgs.fromMap({
   account: CasperHelpers.stringToKey(
-    "013cafb1912c0ca0dc6e0251905f29ebe01176371c298e513a24c0f2d9b2bbff28"
+    "821266406b21b68b4f698efd03df624287e4c195503bc3d9f107a3d829ae6edf"
   ),
 });
 
@@ -43,7 +39,7 @@ const preparedDeploy = contractClient.callEntrypoint(
   "add_admin",
   runtimeArgs,
   key.publicKey,
-  "casper-test",
+  chainName,
   "5000000000",
   [key]
 );
